5fe12a32bdd1ffae62c7635b5a5fa208f195700b
busenurkzlts <118312648+busenurkzlts@users.noreply.github.com>
2023-09-28 20:36:24 +0300

    Add files via upload

---
+++ karar3/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace karar3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 20);
+            label1.TabIndex = 0;
+            label1.Text = "1.Sayıyı Giriniz:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 20);
+            label2.TabIndex = 1;
+            label2.Text = "2.Sayıyı Giriniz:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(77, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Karşılaştır";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(145, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(145, 61);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Red;
+            ClientSize = new Size(319, 150);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ karar3/Form1.cs
@@ -0,0 +1,29 @@
+namespace karar3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte sayi1, sayi2;
+            sayi1 = Convert.ToByte(textBox1.Text);
+            sayi2 = Convert.ToByte(textBox2.Text);
+            if (sayi1 > sayi2)
+            {
+                MessageBox.Show("1.sayý 2.sayýdan büyüktür");
+            }
+            if (sayi1 == sayi2)
+            {
+                MessageBox.Show("sayýlar birbirine eþittir");
+            }
+            if (sayi1 < sayi2)
+            {
+                MessageBox.Show("2.sayý 1.sayýdan büyüktür");
+            }
+        }
+    }
+}
